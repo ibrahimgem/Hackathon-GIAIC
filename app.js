12619a7b10d90c6c754db8d497de5a8f99e6c477
ibrahim <iby.khan7@gmail.com>
2024-09-10 13:39:28 +0500

    Updated HTML, CSS and modified TS
    Updated HTML, CSS and modified TS to add hide/unhide sections functionality in Resume.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function toggleSectionVisibility(sectionClass: string) {
-    const section = document.querySelector(`.${sectionClass}`) as HTMLElement;
+function toggleSectionVisibility(sectionClass) {
+    var section = document.querySelector(".".concat(sectionClass));
     if (section) {
         section.style.display = (section.style.display === 'block' || section.style.display === '') ? 'none' : 'block';
     }
 }
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButtons = {
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButtons = {
         'toggle-about': 'about',
         'toggle-education': 'education',
         'toggle-contact': 'contact',
@@ -13,11 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
         'toggle-work': 'work-experience',
         'toggle-skills': 'skills'
     };
-
-    Object.keys(toggleButtons).forEach(buttonId => {
-        const button = document.getElementById(buttonId);
+    Object.keys(toggleButtons).forEach(function (buttonId) {
+        var button = document.getElementById(buttonId);
         if (button) {
-            button.addEventListener('click', () => {
+            button.addEventListener('click', function () {
                 toggleSectionVisibility(toggleButtons[buttonId]);
             });
         }
